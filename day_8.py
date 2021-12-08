56253eafc59c60849f4f8af9fdea467e75998c27
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-12-08 20:22:55 +0100

    day 8 first attempt

--- day_8.py
+++ day_8.py
@@ -1,11 +1,10 @@
-data = []
-
 def string_filter_function(a_string):
     return len(a_string) > 0
 
 
 def unique_filter_function(a_list):
     return len(a_list) == 1
+
 
 state_dict = {
     0: ['a', 'b', 'c', 'e', 'f', 'g'],
@@ -20,19 +19,28 @@ state_dict = {
     9: ['a', 'b', 'c', 'd', 'f', 'g']
 }
 
-len_dict = {}
 
-for key, values in state_dict.items():
-    if len(values) in len_dict:
-        len_dict[len(values)].append(key)
-    else:
-        len_dict[len(values)] = [key]
+def gen_len_dict(state_dict):
+    len_dict = {}
 
-unique_lens = {}
+    for key, values in state_dict.items():
+        if len(values) in len_dict:
+            len_dict[len(values)].append(key)
+        else:
+            len_dict[len(values)] = [key]
 
-for cnt, keys in len_dict.items():
-    if len(keys) == 1:
-        unique_lens[cnt] = keys[0]
+    return len_dict
+
+
+def gen_unique_lens(len_dict):
+    unique_lens = {}
+
+    for cnt, keys in len_dict.items():
+        if len(keys) == 1:
+            unique_lens[cnt] = keys[0]
+
+    return unique_lens
+
 
 def overlap_function(string_list_a, string_list_b):
     cnt = 0
@@ -43,48 +51,51 @@ def overlap_function(string_list_a, string_list_b):
     return cnt
 
 
-overlap_with_uniques_dict = {}
+def gen_overlap_with_uniques_dict(state_dict):
+    overlap_with_uniques_dict = {}
 
-for state, values in state_dict.items():
-    loc_dict = {}
-    for state_again, values_again in state_dict.items():
-        if not(state_again == state):
-            loc_dict[state_again] = overlap_function(values, values_again)
-    overlap_with_uniques_dict[state] = loc_dict
+    for state, values in state_dict.items():
+        loc_dict = {}
+        for state_again, values_again in state_dict.items():
+            if not(state_again == state):
+                loc_dict[state_again] = overlap_function(values, values_again)
+        overlap_with_uniques_dict[state] = loc_dict
 
-print(overlap_with_uniques_dict)
+    return overlap_with_uniques_dict
 
-overlap_by_length = {}
 
-for key, values in len_dict.items():
-    loc_dict = {}
+def gen_overlap_by_length(len_dict, overlap_with_uniques_dict):
+    overlap_by_length = {}
 
-    for value in values:
-        for overlap_key, overlap_value in overlap_with_uniques_dict[value].items():
-            if overlap_value in loc_dict:
-                if overlap_key in loc_dict[overlap_value]:
-                    loc_dict[overlap_value][overlap_key].append(value)
+    for key, values in len_dict.items():
+        loc_dict = {}
+
+        for value in values:
+            for overlap_key, overlap_value in overlap_with_uniques_dict[value].items():
+                if overlap_value in loc_dict:
+                    if overlap_key in loc_dict[overlap_value]:
+                        loc_dict[overlap_value][overlap_key].append(value)
+                    else:
+                        loc_dict[overlap_value][overlap_key] = [value]
                 else:
-                    loc_dict[overlap_value][overlap_key] = [value]
-            else:
-                loc_dict[overlap_value] = {overlap_key: [value]}
+                    loc_dict[overlap_value] = {overlap_key: [value]}
 
-    overlap_by_length[key] = loc_dict
+        overlap_by_length[key] = loc_dict
 
-print(overlap_by_length)
+    return overlap_by_length
 
 
-with open("/Users/jonasvandenbulcke/Documents/reps/advent_of_code_2021/data/day_8.txt", 'r') as input_data:
-    data = [[list(filter(string_filter_function, data.split(' '))) for data in string.split('|')] for string in input_data.read().split('\n')]
+def search_overlap_by_length(string, solved_string, solved_strings_value, overlap_by_length):
+    string_length = len(string)
+    overlap_strings = overlap_function(string, solved_string)
 
-unique_value_count = dict((value, 0) for value in unique_lens.values())
+    return overlap_by_length[string_length][overlap_strings][solved_strings_value]
 
-def search_overlap_by_length(string, solved_string, overlap_by_length):
-    pass
 
 def solve_output_strings(outputs, unique_value_count, unique_lens, overlap_by_length):
     output_string = []
     solved = []
+    solved_strings_values = []
     not_solved = []
 
     # filtering the unique ones
@@ -95,24 +106,83 @@ def solve_output_strings(outputs, unique_value_count, unique_lens, overlap_by_le
             solved.append(string)
             unique_value_count[unique_lens[len(string)]] += 1
             output_string.append(str(unique_lens[len(string)]))
+            solved_strings_values.append(unique_lens[len(string)])
 
         else:
 
             not_solved.append(string)
             output_string.append(string)
 
-    print("before solving based on found uniques: " + ' '.join(output_string))
+    # print("before solving based on found uniques: " + ' '.join(output_string))
+
+    print(not_solved)
+    print(solved)
 
     count = 20
     while any(not_solved):
-        for string in not_solved:
-            for solved_string in solved:
-                pass
+        new_not_solved = not_solved[:]
+        new_solved = solved[:]
+
+        for i, string in enumerate(not_solved):
+            for j, solved_string in enumerate(solved):
+                keys = search_overlap_by_length(
+                    string,
+                    solved_string,
+                    solved_strings_values[j],
+                    overlap_by_length
+                )
+
+                if len(keys) == 1:
+                    print(keys, i, j, solved, not_solved)
+                    unique_value_count[keys[0]] += 1
+                    solved_strings_values.append(keys[0])
+                    new_solved.append(string)
+                    new_not_solved.pop(i)
+
+                    # print(new_not_solved)
+
+                    print("found a new solution!")
+
+                    break
+
+        not_solved = new_not_solved
+        solved = new_solved
+
+        count -= 1
+
+        print(count)
+
+        if count < 0:
+            print("over-itterated")
+            break
 
 
+if __name__ == "__main__":
+    data = []
 
-for input_data, _ in data:
-    solve_output_strings(input_data, unique_value_count, unique_lens, overlap_by_length)
+    with open("data/day_8.txt", 'r') as input_data:
+        data = [[tuple(filter(string_filter_function, loc_data.split(' '))) for loc_data in string.split('|')] for string in input_data.read().split('\n')]
 
-print(unique_value_count)
-print(sum(unique_value_count.values()))
+    len_dict = gen_len_dict(state_dict)
+
+    print(len_dict)
+
+    unique_lens = gen_unique_lens(len_dict)
+    print(unique_lens)
+
+    unique_value_count = dict((value, 0) for value in range(10))
+    print(unique_value_count)
+
+    overlap_with_uniques_dict = gen_overlap_with_uniques_dict(state_dict)
+    print(overlap_with_uniques_dict)
+
+    overlap_by_length = gen_overlap_by_length(len_dict, overlap_with_uniques_dict)
+
+    for input_data, output_data in data:
+        solve_output_strings(output_data, unique_value_count, unique_lens, overlap_by_length)
+
+    print(unique_value_count)
+    print(sum(unique_value_count.values()))
+
+    for input_data, output_data in data:
+        solve_output_strings(input_data, unique_value_count, unique_lens, overlap_by_length)
