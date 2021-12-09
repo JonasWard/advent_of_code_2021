7c7be294e6c58a3bdeb84962d084c695fc911c1a
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-12-09 15:11:10 +0100

    day_9

--- day_9.py
+++ day_9.py
@@ -1,70 +1,108 @@
-def i_curtailing(i, count):
-    if i == 0:
-        return [], [1, 2]
-    elif i == 1:
-        return [0], [2, 3]
-    elif i == count - 2:
-        return [count - 4, count - 3], [count - 1]
-    elif i == count - 1:
-        return [count - 3, count - 2], []
-    else:
-        return [i - 2, i - 1], [i + 1, i + 2]
+def calculate_smallest_values(grid):
+    smallest_values = []
+
+    m = len(grid) - 1
+    n = len(grid[0]) - 1
+
+    risk_count = 0
+
+    print(m, n)
+
+    for j, row in enumerate(grid):
+        for i, v in enumerate(row):
+            if i == 0:
+                if j == 0:
+                    other_vs = [(0, 1), (1, 0)]
+                elif j == m:
+                    other_vs = [(m - 1, 0), (m, 1)]
+                else:
+                    other_vs = [(j - 1, 0), (j, 1), (j + 1, 0)]
+            elif i == n:
+                if j == 0:
+                    other_vs = [(0, n-1), (1, n)]
+                elif j == m:
+                    other_vs = [(m - 1, n), (m, n-1)]
+                else:
+                    other_vs = [(j - 1, n), (j, n-1), (j + 1, n)]
+            else:
+                if j == 0:
+                    other_vs = [(0, i-1), (1, i), (0, i+1)]
+                elif j == m:
+                    other_vs = [(m - 1, i), (m, i-1), (m, i+1)]
+                else:
+                    other_vs = [(j - 1, i), (j, i-1), (j, i+1), (j + 1, i)]
+
+            smaller_count = 0
+
+            for y, x in other_vs:
+                if grid[y][x] > grid[j][i]:
+                    smaller_count += 1
+
+            if smaller_count == len(other_vs):
+                print((j, i), other_vs)
+
+                smallest_values.append((j, i))
+                risk_count += v + 1
+
+    return risk_count, smallest_values
 
 
-def finding_smallest(main_row):
-    # row_min_2 = main_row[2:] + [0, 0]
-    # row_min_1 = main_row[1:] + [0]
-    #
-    # row_max_2 = [0, 0] + main_row[:-2]
-    # row_max_1 = [0] + main_row[:-1]
-    #
-    # row_deltas = [0 for i in range(len(main_row))]
-    #
-    # for row_section in [row_min_2, row_min_1, row_max_1, row_max_2]:
-    #     for i, v in enumerate(row_section):
-    #         row_deltas[i] += main_row[i] - v
-    #
-    solution_key = []
+def grid_activating(grid):
+    m, n = len(grid), len(grid[0])
 
-    total_risk = 0
+    bool_grid = []
+    for j in range(m):
+        row_grid = []
+        for i in range(n):
+            row_grid.append(grid[j][i] != 9)
+        bool_grid.append(row_grid)
 
-    for i, v in enumerate(main_row):
-        smaller_is, larger_is = i_curtailing(i, len(main_row))
+    return bool_grid
 
-        if len(smaller_is) == 2 and len(larger_is) == 2:
-            v0, v1 = [main_row[i] for i in smaller_is]
-            v2, v3 = [main_row[i] for i in larger_is]
 
-            if (v1 < v0) and (v1 > v) and (v < v2) and (v2 < v3):
-                total_risk += v + 1
+def calculate_basin(well, grid, bool_grid):
+    j, i = well
 
-        elif len(smaller_is) == 2 and len(larger_is) == 1:
-            v0, v1 = [main_row[i] for i in smaller_is]
-            v2 = [main_row[i] for i in larger_is][0]
+    print(well, grid[j][i])
+    bool_grid[j][i] = False
 
-            if (v1 < v0) and (v1 > v) and (v < v2):
-                total_risk += v + 1
+    well_cnt = 1
 
-        elif len(smaller_is) == 2 and len(larger_is) == 0:
-            v0, v1 = [main_row[i] for i in smaller_is]
+    if j - 1 >= 0:
+        if bool_grid[j - 1][i] and grid[j - 1][i] > grid[j][i]:
+            bool_grid[j - 1][i] = False
+            fnd_cnt = calculate_basin((j - 1, i), grid, bool_grid)
+            well_cnt += fnd_cnt
 
-            if (v1 < v0) and (v1 > v):
-                total_risk += v + 1
+    if j + 1 <= len(grid) - 1:
+        if bool_grid[j + 1][i] and grid[j + 1][i] > grid[j][i]:
+            bool_grid[j + 1][i] = False
+            fnd_cnt = calculate_basin((j + 1, i), grid, bool_grid)
+            well_cnt += fnd_cnt
 
-        elif len(smaller_is) == 0 and len(larger_is) == 2:
-            v2, v3 = [main_row[i] for i in larger_is]
+    if i - 1 >= 0:
+        if bool_grid[j][i - 1] and grid[j][i - 1] > grid[j][i]:
+            bool_grid[j][i - 1] = False
+            fnd_cnt = calculate_basin((j, i - 1), grid, bool_grid)
+            well_cnt += fnd_cnt
 
-            if (v < v2) and (v2 < v3):
-                total_risk += v + 1
+    if i + 1 <= len(grid[0]) - 1:
+        if bool_grid[j][i + 1] and grid[j][i + 1] > grid[j][i]:
+            bool_grid[j][i + 1] = False
+            fnd_cnt = calculate_basin((j, i + 1), grid, bool_grid)
+            well_cnt += fnd_cnt
 
-        elif len(smaller_is) == 1 and len(larger_is) == 2:
-            v1 = [main_row[i] for i in smaller_is][0]
-            v2, v3 = [main_row[i] for i in larger_is]
+    return well_cnt
 
-            if (v1 > v) and (v < v2) and (v2 < v3):
-                total_risk += v + 1
 
-    return total_risk
+def grid_searching(grid, smallest_values):
+    bool_grid = grid_activating(grid)
+
+    basins = {}
+    for pair in smallest_values:
+        basins[pair] = calculate_basin(pair, grid, bool_grid)
+
+    return basins
 
 
 if __name__ == "__main__":
@@ -73,10 +111,17 @@ if __name__ == "__main__":
     with open("data/day_9.txt", 'r') as input_data:
         data = [[int(v) for v in string] for string in input_data.read().split('\n')]
 
-    total_risk = 0
+    # total_risk = 0
 
-    for row in data:
-        # print(''.join([str(v) for v in row]))
-        total_risk += finding_smallest(row)
+    total_risk, smallest_values = calculate_smallest_values(data)
+
+    basins = grid_searching(data, smallest_values[:])
+
+    a_set = list(basins.values())
+    a_set.sort()
+
+    print(a_set[-3], a_set[-2], a_set[-1])
+    print(a_set[-3] * a_set[-2] * a_set[-1])
 
     print(total_risk)
+    print(basins)
